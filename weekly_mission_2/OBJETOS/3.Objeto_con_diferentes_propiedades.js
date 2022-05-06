599d6ffd1b27845d0557e90afabf62e1117a11f2
skrillheaven <beto.jacp2010@gmail.com>
2022-05-06 18:03:57 -0500

    creando objeto con difrerentes propiedades

---
+++ weekly_mission_2/OBJETOS/3.Objeto_con_diferentes_propiedades.js
@@ -0,0 +1,20 @@
+console.log("objetos")
+    // Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Tulio",
+    age: 2,
+    nicknames: [
+        "Tulipan",
+        "Tulancingo",
+        "Tulish"
+    ],
+    address: {
+        zip_code: "10000",
+        street: "Dr. Vertiz 11 Benito Juarez",
+        city: "CDMX"
+    }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3) // se imprime todo el objeto
+console.log(myObject3.name) // se imprime el nombre
+console.log(myObject3["address"]) // se imprime lo que contiene addres
