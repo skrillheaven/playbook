80c186131750016577fb1580fe9d38ca68068c34
skrillheaven <beto.jacp2010@gmail.com>
2022-05-06 17:45:32 -0500

    creando un objeto con propiedades

---
+++ weekly_mission_2/OBJETOS/2.Crear_objeto_con_propiedades.js
@@ -0,0 +1,10 @@
+console.log("Objetos")
+
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Carlo", //nombre
+    age: 27 //edad
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log("Propiedades del objeto" +
+    myObjetc2)
