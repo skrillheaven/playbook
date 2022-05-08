28b4f6bf00e03badb7020050c2d06f53af5b71eb
skrillheaven <beto.jacp2010@gmail.com>
2022-05-08 12:25:49 -0500

    ForEach:Calculando la suma de una lista

---
+++ weekly_mission_2/OPERADORES/2.For_each_para_calcular_la_suma_de_una_lista.js
@@ -0,0 +1,6 @@
+// Ejemplo 2: for Each para calcular la suma de todos los elementos de una list
+let sum = 0
+const numbers2 = [1, 2, 3, 4, 5]; //lista
+numbers2.forEach(num => sum += num) //se lee la lista y se suman los datos en sum
+console.log("Ejemplo2: Calculo de la suma de los numeros de una lista")
+console.log(sum)
