1cd03f516c27e0365ed1bf4abf794a9add5f4f92
skrillheaven <beto.jacp2010@gmail.com>
2022-05-08 12:32:14 -0500

    FOREACH: para imprimir los países en letras mayusculas

---
+++ weekly_mission_2/OPERADORES/3.ForEach_para_imprimir_los_paises_en_mayusculas.js
@@ -0,0 +1,5 @@
+// Ejemplo 3: forEach para imprimir los países en letras mayúsculas
+
+const countries = ['finland', 'Denmak', 'Sweden', 'Norway', 'Iceland'] //lista
+console.log("Ejemplo3: Imprimiendo la lista de países en mayusculas")
+countries.forEach((element) => console.log(element.toUpperCase())) //element se almacena la lista,touppcase, para poner mayuscula
