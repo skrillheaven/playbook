c58195437f13c26636ba9a1e9ece87a8cb2572fb
skrillheaven <beto.jacp2010@gmail.com>
2022-05-08 13:17:54 -0500

    uso de map para convertir en minusculas

---
+++ weekly_mission_2/OPERADORES/5.Uso_de_map_para_convertir_lista_en_minuscula.js
@@ -0,0 +1,7 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas
+const names = ['beto', 'pepe', 'diego', 'diana ']
+
+const namesToUpperCase = names.map((name1) => name1.toLowerCase()) //<--is lowercase
+
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
+console.log(namesToUpperCase)
