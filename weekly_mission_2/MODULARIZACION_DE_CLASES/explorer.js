f3d64a68744bbe2d2ba52327e032d1cffb49f4ae
skrillheaven <beto.jacp2010@gmail.com>
2022-05-08 21:40:13 -0500

    Creando clase padre: 'EXPLORER'

---
+++ weekly_mission_2/MODULARIZACION_DE_CLASES/explorer.js
@@ -0,0 +1,12 @@
+//clase padre, en la cual trabajaremos mas adelante 
+export default class Explorer {
+    constructor(name, username, mission) {
+        this.name = name
+        this.username = username
+        this.mission = mission
+    }
+
+    getNameAndUsername() {
+        return `Explorer ${this. name}, username: ${this.username}`
+    }
+}
