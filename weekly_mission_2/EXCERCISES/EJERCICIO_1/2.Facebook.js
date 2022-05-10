060015ff35d608ebcd750e85b60100b0a7ad508d
skrillheaven <beto.jacp2010@gmail.com>
2022-05-09 21:42:49 -0500

    agregando objeto post para facebook

--- weekly_mission_2/EXCERCISES/EJERCICIO_1/2.Facebook.js
+++ weekly_mission_2/EXCERCISES/EJERCICIO_1/2.Facebook.js
@@ -8,7 +8,31 @@ const user = {
     }
 }
 
+const post = {
+    nameuser: user.name,
+    date: new Date(),
+    title: "My little, beautiful dog ↓↓",
+    comments: 47,
+    reactions: 12,
+    getAuthorandTitle: function() {
+        return `Your friend ${this.nameuser} \njust posted something new!!!! ${this.title}`
+    },
+    getDate: function() {
+        return `Date Time: ${this.date}`
+    }
+}
+
+
+
+
+
 
 console.log("USER")
 console.log(user)
 console.log(user.getname())
+
+
+console.log("\nPOST")
+console.log(post)
+console.log(post.getAuthorandTitle())
+console.log(post.getDate())
