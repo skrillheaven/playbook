31a588025016c42ea5a5b97a33c1d0f4585065ce
skrillheaven <beto.jacp2010@gmail.com>
2022-05-29 18:15:45 -0500

    Imprimiendo nombre y stack con for each en un objeto con muchos datos

---
+++ weekly_mission_2/EXCERCISES/EJERCICIO_2/explorer.js
@@ -0,0 +1,67 @@
+const explorers = [{
+        name: "Explorer 1",
+        exercises_completed: 10,
+        rate: 99,
+        city: "CDMX",
+        stack: [
+            "js",
+            "c#"
+        ],
+        missions: {
+            onboarding: {
+                isFinished: true,
+                exercisesFinished: true
+            },
+            frontend: {
+                isFinished: true,
+                exercisesFinished: true
+            }
+        }
+    },
+    {
+        name: "Explorer 2",
+        exercises_completed: 9,
+        city: "Veracruz",
+        rate: 50,
+        stack: [
+            "js"
+        ],
+        missions: {
+            onboarding: {
+                isFinished: false,
+                exercisesFinished: false
+            },
+            frontend: {
+                isFinished: false,
+                exercisesFinished: false
+            }
+        }
+    },
+    {
+        name: "Explorer 3",
+        exercises_completed: 3,
+        city: "Sonora",
+        rate: 100,
+        stack: [
+            "elixir"
+        ],
+        missions: {
+            onboarding: {
+                isFinished: true,
+                exercisesFinished: true
+            },
+            frontend: {
+                isFinished: false,
+                exercisesFinished: false
+            }
+        }
+    }
+]
+
+
+
+console.log("Imprimiendo nombre y stack del explorer con for each")
+explorers.forEach(explorer => console.log("Nombre del explorer : " +
+        explorer.name + "\nStack:" +
+        explorer.stack))
+    //explorers.forEach(explorer => console.log(explorer.stack)) otra manera de hacerlo
